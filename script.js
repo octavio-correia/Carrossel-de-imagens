e7049bcdccf9287a5fe568ce194430bb1f8b99b3
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-26 13:00:18 -0300

    Testes responsividade

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function frente() {
         idx = 0
     }
 
-    imgs.style.transform = `translateX(${-idx *400}px)`
+    imgs.style.transform = `translateX(${-idx * 400}px)`
 }
 
 function tras() {
@@ -18,7 +18,7 @@ function tras() {
         idx = img.length - 1
     }
 
-    imgs.style.transform = `translateX(${-idx *400}px)`
+    imgs.style.transform = `translateX(${-idx * 400}px)`
 }
 
 //TEXTOS
@@ -30,51 +30,60 @@ var idx = 0
 
 function frentetxt() {
 
-    txts.style.transform = `translateX(${-idx *700}px)`
+    txts.style.transform = `translateX(${-idx * 700}px)`
 }
 
 function trastxt() {
-    txts.style.transform = `translateX(${-idx *700}px)`
+    txts.style.transform = `translateX(${-idx * 700}px)`
 }
 
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     // Obter a largura atual da janela
     var windowWidth = window.innerWidth;
-  
+
     // Verificar a largura da janela e realizar alterações responsivas
     if (windowWidth < 768) {
-      // Alterações para telas menores que 768px de largura
-      // Exemplo:
+        // Alterações para telas menores que 768px de largura
+        const imgs = document.getElementById("img")
+        const img = document.querySelectorAll("#img img")
 
-      function frente() {
-        idx++
-        if (idx > img.length - 1) {
-            idx = 0
+        var idx = 0
+
+        function frente() {
+            idx++
+            if (idx > img.length - 1) {
+                idx = 0
+            }
+
+            imgs.style.transform = `translateX(${-idx * 200}px)`
         }
-    
-        imgs.style.transform = `translateX(${-idx *400}px)`
-    }
-    
-    function tras() {
-        idx--
-        if (idx < 0) {
-            idx = img.length - 1
+
+        function tras() {
+            idx--
+            if (idx < 0) {
+                idx = img.length - 1
+            }
+
+            imgs.style.transform = `translateX(${-idx * 200}px)`
         }
-    
-        imgs.style.transform = `translateX(${-idx *400}px)`
-    }
-    
 
-      function frentetxt() {
+        //TEXTOS
 
-        txts.style.transform = `translateX(${-idx *350}px)`
-    }
-    
-    function trastxt() {
-        txts.style.transform = `translateX(${-idx *350}px)`
-    }
+        const txts = document.getElementById("txt")
+        const txt = document.querySelectorAll("#txt div")
 
-      
+        var idx = 0
+
+        function frentetxt() {
+
+            txts.style.transform = `translateX(${-idx * 350}px)`
+        }
+
+        function trastxt() {
+            txts.style.transform = `translateX(${-idx * 350}px)`
+        }
+
     }
-})
+}
+)

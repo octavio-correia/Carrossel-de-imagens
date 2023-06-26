4f0c899273eaeb4c0b4615cfa09f5b47d44668d3
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-25 22:49:06 -0300

    Ajustes de responsividade

--- script.js
+++ script.js
@@ -36,3 +36,45 @@ function frentetxt() {
 function trastxt() {
     txts.style.transform = `translateX(${-idx *700}px)`
 }
+
+
+window.addEventListener('resize', function() {
+    // Obter a largura atual da janela
+    var windowWidth = window.innerWidth;
+  
+    // Verificar a largura da janela e realizar alterações responsivas
+    if (windowWidth < 768) {
+      // Alterações para telas menores que 768px de largura
+      // Exemplo:
+
+      function frente() {
+        idx++
+        if (idx > img.length - 1) {
+            idx = 0
+        }
+    
+        imgs.style.transform = `translateX(${-idx *400}px)`
+    }
+    
+    function tras() {
+        idx--
+        if (idx < 0) {
+            idx = img.length - 1
+        }
+    
+        imgs.style.transform = `translateX(${-idx *400}px)`
+    }
+    
+
+      function frentetxt() {
+
+        txts.style.transform = `translateX(${-idx *700}px)`
+    }
+    
+    function trastxt() {
+        txts.style.transform = `translateX(${-idx *700}px)`
+    }
+
+      
+    }
+})

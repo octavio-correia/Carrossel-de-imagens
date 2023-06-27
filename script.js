adefc847fc41cdba0e70404111ec3a0051236ad8
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-26 22:34:40 -0300

    Versão 2.0
    Nessa versão o site está responsivo. Tive que acrescentar um button novo para poder executá-lo no modo responsivo

--- script.js
+++ script.js
@@ -21,15 +21,12 @@ function tras() {
     imgs.style.transform = `translateX(${-idx * 400}px)`
 }
 
-//TEXTOS
-
 const txts = document.getElementById("txt")
 const txt = document.querySelectorAll("#txt div")
 
 var idx = 0
 
 function frentetxt() {
-
     txts.style.transform = `translateX(${-idx * 700}px)`
 }
 
@@ -37,53 +34,58 @@ function trastxt() {
     txts.style.transform = `translateX(${-idx * 700}px)`
 }
 
+// ----------------------RESPONSIVO-----------------------------//
+
+var idx = 0
+
+function frenteR() {
+    idx++
+    if (idx > img.length - 1) {
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 200}px)`
+}
+
+function trasR() {
+    idx--
+    if (idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 200}px)`
+}
+
+//TEXTOS
+
+var idx = 0
+
+function frentetxtR() {
+
+    txts.style.transform = `translateX(${-idx * 330}px)`
+}
+
+function trastxtR() {
+    txts.style.transform = `translateX(${-idx * 330}px)`
+}
+
 
 window.addEventListener('resize', function () {
     // Obter a largura atual da janela
     var windowWidth = window.innerWidth;
-
-    // Verificar a largura da janela e realizar alterações responsivas
     if (windowWidth < 768) {
-        // Alterações para telas menores que 768px de largura
-        const imgs = document.getElementById("img")
-        const img = document.querySelectorAll("#img img")
-
-        var idx = 0
-
-        function frente() {
-            idx++
-            if (idx > img.length - 1) {
-                idx = 0
-            }
-
-            imgs.style.transform = `translateX(${-idx * 200}px)`
-        }
-
-        function tras() {
-            idx--
-            if (idx < 0) {
-                idx = img.length - 1
-            }
-
-            imgs.style.transform = `translateX(${-idx * 200}px)`
-        }
-
+        // Executar função quando a largura for menor que 768
         //TEXTOS
+        frenteR()
+        frentetxtR()
+        trasR()
+        trastxtR()
 
-        const txts = document.getElementById("txt")
-        const txt = document.querySelectorAll("#txt div")
-
-        var idx = 0
-
-        function frentetxt() {
-
-            txts.style.transform = `translateX(${-idx * 350}px)`
-        }
-
-        function trastxt() {
-            txts.style.transform = `translateX(${-idx * 350}px)`
-        }
-
+    } else {
+        // Executar outra função quando a largura for maior ou igual a 768
+        frente()
+        frentetxt()
+        tras()
+        trastxt()
     }
-}
-)
+});
